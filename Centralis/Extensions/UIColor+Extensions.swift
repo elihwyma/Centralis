9932e1659759fe3f75d646f7614a85d1efc7d2d2
Amy While <mrcharliewhile@gmail.com>
2020-12-17 22:16:15 +0000

    Create UIColor+Extensions.swift

---
+++ Centralis/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extensions.swift
+//  Centralis
+//
+//  Created by Amy While on 17/12/2020.
+//
+
+import UIKit
+
+extension UIColor {
+    class func randomColor(randomAlpha: Bool = false) -> UIColor {
+        let redValue = CGFloat(arc4random_uniform(255)) / 255.0;
+        let greenValue = CGFloat(arc4random_uniform(255)) / 255.0;
+        let blueValue = CGFloat(arc4random_uniform(255)) / 255.0;
+        let alphaValue = randomAlpha ? CGFloat(arc4random_uniform(255)) / 255.0 : 1;
+
+        return UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: alphaValue)
+    }
+}
+
